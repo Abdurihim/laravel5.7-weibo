7a4d2fd46c4cb23bd5e653fce80340c8dfaa2d48
Abdurihim <a17738725971@163.com>
2021-02-16 00:40:42 +0800

    静态文件浏览器缓存问题

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,8 @@
 @section('content')
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
-    <p class="lead">你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-tra">laravel入门教程</a>的示例项目主页</p>
+    <p class="lead">你现在所看到的是 <a href="https://laravel-china.org/courses/laravel-essential-tra">laravel入门教程</a>的示例项目主页
+    </p>
     <p>
       一切，将从这里开始。
     </p>
