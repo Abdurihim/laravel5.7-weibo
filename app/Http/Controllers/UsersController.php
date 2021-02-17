5d1032080d5fd66f876bd613bdbe410d20cf3c26
Abdurihim <a17738725971@163.com>
2021-02-17 23:09:46 +0800

    访问策略

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,7 +7,20 @@ use Auth;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
+
+
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+
+    }
+
     //
     public function create()
     {
@@ -41,11 +54,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(User $user, Request $request)
     {
+        $this->authorize('update', $user);
         $this->validate($request, [
             "name" => "required|max:50",
             "password" => "nullable|confirmed|min:6"
