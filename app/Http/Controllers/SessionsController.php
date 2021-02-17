818f9ac5d16a53f1ab86447b664e41099de0081b
Abdurihim <a17738725971@163.com>
2021-02-17 20:13:44 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -33,4 +33,12 @@ class SessionsController extends Controller
         }
 
     }
+
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+
 }
